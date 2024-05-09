307722824c9504098ba80b1448dc33f5010a1982
LucasXColaco <133161894+LucasXColaco@users.noreply.github.com>
2024-05-09 11:40:06 -0300

    Initial commit

---
+++ assets/scripts/script.js
@@ -0,0 +1,15 @@
+function openMenu() {
+  menu.style.display = "flex";
+  menu.style.animation = "fadein 0.2s ease-in";
+  document.body.style.overflowY = "hidden";
+  whatsapp.style.display = "none";
+  whatsapp.style.animation = "fadeout 0.2s ease-out";
+}
+
+function closeMenu() {
+  menu.style.animation = "fadeoutmenu 0.2s ease-out";
+  menu.style.display = "none";
+  document.body.style.overflowY = "visible";
+  whatsapp.style.display = "block";
+  whatsapp.style.animation = "fadein 0.2s ease-in";
+}
